89af4f7d1777d84cfa1d7e64ae4da8b02a91fdb2
Maha Girinda <putu.praba@sigma.co.id>
2024-05-22 09:34:37 +0800

    penambahan middleware

---
+++ app/Http/Middleware/RoleMiddleware.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
+use Symfony\Component\HttpFoundation\Response;
+
+class RoleMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  Closure(Request): (Response)  $next
+     */
+    public function handle(Request $request, Closure $next, $role): Response
+    {
+        Log::info("URL yang diakses adalah " . $request->url());
+
+        // $is_authenticated = Auth::check();
+        $is_authenticated = true;
+
+        if (!$is_authenticated) {
+            return response('Anda login terlebih dahulu!', 401);
+        }
+
+        // $user_role = Auth::user()->role;
+        $user_role = "admin";
+        if ($user_role != $role && $role != 'all') {
+            return response('Hanya bisa diakses oleh admin!', 401);
+        }
+
+        return $next($request);
+    }
+}
