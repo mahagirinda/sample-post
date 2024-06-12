afc305b123d0d67fd9220831bed056f51d0ebd5c
Maha Girinda <putu.praba@sigma.co.id>
2024-06-12 11:49:16 +0800

    mengubah seeder agar bisa dijalankan tanpa parameter class

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Category;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +14,73 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        /**
+         * Initial Category Data Seed
+         */
+        $categories = [
+            [
+                'name' => 'Kuliner',
+                'detail' => 'Kategori tentang makanan dan minuman',
+                'status' => true,
+            ],
+            [
+                'name' => 'Binatang',
+                'detail' => 'Kategori tentang hewan dan fauna',
+                'status' => true,
+            ],
+            [
+                'name' => 'Gaya Hidup',
+                'detail' => 'Kategori tentang gaya hidup dan tren',
+                'status' => true,
+            ],
+            [
+                'name' => 'Entertainment',
+                'detail' => 'Kategori tentang hiburan dan seni',
+                'status' => true,
+            ],
+            [
+                'name' => 'Education',
+                'detail' => 'Kategori tentang edukasi dan pendidikan',
+                'status' => true,
+            ],
+            [
+                'name' => 'Art',
+                'detail' => 'Kategori tentang karya seni',
+                'status' => true,
+            ],
+            [
+                'name' => 'Tips & Trick',
+                'detail' => 'Kategori tentang tips dan trik',
+                'status' => true,
+            ],
+        ];
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        foreach ($categories as $category) {
+            Category::create($category);
+        }
+
+        /**
+         * Initial User Data Seed if not on production
+         */
+        if(!env('APP_ENV') == 'production') {
+            $users = [
+                [
+                    'role' => 'admin',
+                    'name' => 'Admin User',
+                    'email' => 'admin@example.com',
+                    'password' => Hash::make('password'),
+                ],
+                [
+                    'role' => 'user',
+                    'name' => 'Regular User',
+                    'email' => 'user@example.com',
+                    'password' => Hash::make('password'),
+                ],
+            ];
+
+            foreach ($users as $user) {
+                User::create($user);
+            }
+        }
     }
 }
