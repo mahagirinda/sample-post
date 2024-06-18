ee7df9a937868f5ced94a281feafee02adf1d3ee
Maha Girinda <putu.praba@sigma.co.id>
2024-06-18 11:48:09 +0800

    penambahan view login, register, dan forget password

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,3 +1,146 @@
-<div>
-    <!-- Simplicity is the consequence of refined emotions. - Jean D'Alembert -->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <link rel="shortcut icon" href="{{ url("images/icon.png") }}" type="image/x-icon"/>
+    <title>{{ env('APP_NAME') }} - Login</title>
+
+    <link rel="stylesheet" href="{{ url("css/bootstrap.min.css") }}"/>
+    <link rel="stylesheet" href="{{ url("css/lineicons.css") }}" type="text/css"/>
+    <link rel="stylesheet" href="{{ url("css/main.css") }}"/>
+    <link rel="stylesheet" href="{{ url("css/additional.css") }}"/>
+</head>
+
+<body>
+<div id="preloader">
+    <div class="spinner"></div>
 </div>
+
+<div class="container-md">
+    <section class="signin-section">
+        <div class="container-fluid">
+            <div class="title-wrapper pt-30">
+                <div class="row align-items-center">
+                    <div class="col-md-6">
+                        <div class="title">
+                            <h2>Simple Post</h2>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="breadcrumb-wrapper">
+                            <nav aria-label="breadcrumb">
+                                <ol class="breadcrumb">
+                                    <li class="breadcrumb-item">
+                                        Login
+                                    </li>
+                                </ol>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row g-0 auth-row">
+                <div class="col-lg-6">
+                    <div class="auth-cover-wrapper bg-primary-100">
+                        <div class="auth-cover">
+                            <div class="title text-center">
+                                <h1 class="text-primary mb-10">Welcome Back</h1>
+                                <p class="text-medium">
+                                    Sign in to your Existing account to continue!
+                                </p>
+                            </div>
+                            <div class="cover-image">
+                                <img src="{{ url('images/auth/signin-image.svg') }}" alt=""/>
+                            </div>
+                            <div class="shape-image">
+                                <img src="{{ url('images/auth/shape.svg') }}" alt=""/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- end col -->
+                <div class="col-lg-6">
+                    <div class="signin-wrapper">
+                        <div class="form-wrapper">
+                            <h6 class="mb-15">Log In Form</h6>
+                            <p class="text-sm mb-25">
+                                Let's create a post! Log in to the the app now!
+                            </p>
+                            <form action="{{ route('login') }}" method="POST">
+                                @csrf
+                                <div class="row">
+                                    <div class="col-12">
+                                        <div class="input-style-1">
+                                            <label>{{ __('Email Address') }}</label>
+                                            <input class="form-control @error('email') is-invalid @enderror"
+                                                   type="email"
+                                                   placeholder="Email" name="email"/>
+
+                                            @error('email')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                            @enderror
+                                        </div>
+                                    </div>
+                                    <div class="col-12">
+                                        <div class="input-style-1">
+                                            <label>{{ __('Password') }}</label>
+                                            <input type="password"
+                                                   class="form-control @error('password') is-invalid @enderror"
+                                                   placeholder="Password" name="password" required
+                                                   autocomplete="current-password"/>
+                                            @error('password')
+                                            <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                            @enderror
+                                        </div>
+                                    </div>
+                                    <div class="col-xxl-6 col-lg-12 col-md-6">
+                                        <div class="form-check checkbox-style mb-30">
+                                            <input class="form-check-input" type="checkbox" name="remember"
+                                                   id="remember" {{ old('remember') ? 'checked' : '' }}>
+                                            <label class="form-check-label"
+                                                   for="remember">{{ __('Remember Me') }}</label>
+                                        </div>
+                                    </div>
+                                    <div class="col-xxl-6 col-lg-12 col-md-6">
+                                        <div class="text-start text-md-end text-lg-start text-xxl-end mb-30">
+                                            <a href="{{ route('password.request') }}" class="hover-underline">
+                                                Forgot Password?
+                                            </a>
+                                        </div>
+                                    </div>
+                                    <div class="col-12">
+                                        <div class="button-group d-flex justify-content-center flex-wrap">
+                                            <button class="main-btn primary-btn btn-hover w-100 text-center">
+                                                Log In
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                            <div class="singin-option pt-40">
+                                <p class="text-sm text-medium text-dark text-center">
+                                    Don’t have any account yet?
+                                    <a href="{{ route('register') }}">Create an account</a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
+
+<script src="{{ url("js/bootstrap.bundle.min.js") }}"></script>
+<script src="{{ url("js/main.js") }}"></script>
+</body>
+
+</html>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,3 +1,152 @@
-<div>
-    <!-- An unexamined life is not worth living. - Socrates -->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <link rel="shortcut icon" href="{{ url("images/icon.png") }}" type="image/x-icon"/>
+    <title>{{ env('APP_NAME') }} - Login</title>
+
+    <link rel="stylesheet" href="{{ url("css/bootstrap.min.css") }}"/>
+    <link rel="stylesheet" href="{{ url("css/lineicons.css") }}" type="text/css"/>
+    <link rel="stylesheet" href="{{ url("css/main.css") }}"/>
+    <link rel="stylesheet" href="{{ url("css/additional.css") }}"/>
+</head>
+
+<body>
+<div id="preloader">
+    <div class="spinner"></div>
 </div>
+
+<div class="container-md">
+    <section class="signin-section">
+        <div class="container-fluid">
+            <div class="title-wrapper pt-30">
+                <div class="row align-items-center">
+                    <div class="col-md-6">
+                        <div class="title">
+                            <h2>Simple Post</h2>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="breadcrumb-wrapper">
+                            <nav aria-label="breadcrumb">
+                                <ol class="breadcrumb">
+                                    <li class="breadcrumb-item">
+                                        Register
+                                    </li>
+                                </ol>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row g-0 auth-row">
+                <div class="col-lg-6">
+                    <div class="auth-cover-wrapper bg-primary-100">
+                        <div class="auth-cover">
+                            <div class="title text-center">
+                                <h1 class="text-primary mb-10">Get Started</h1>
+                                <p class="text-medium">
+                                    Start creating account, and connect via Post
+                                    <br class="d-sm-block"/>
+                                    to others.
+                                </p>
+                            </div>
+                            <div class="cover-image">
+                                <img src="{{ url('images/auth/signin-image.svg') }}" alt=""/>
+                            </div>
+                            <div class="shape-image">
+                                <img src="{{ url('images/auth/shape.svg') }}" alt=""/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="signup-wrapper">
+                        <div class="form-wrapper">
+                            <h6 class="mb-15">Register Form</h6>
+                            <p class="text-sm mb-25">
+                                Let's create a post! Register to the the app now!
+                            </p>
+                            <form method="POST" action="{{ route('register') }}">
+                                @csrf
+                                <div class="row">
+                                    <div class="col-12">
+                                        <div class="input-style-1">
+                                            <label>Name</label>
+                                            <input id="name" type="text"
+                                                   class="form-control @error('name') is-invalid @enderror" name="name"
+                                                   value="{{ old('name') }}" required autocomplete="name"
+                                                   autofocus>
+                                            @error('name')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                            @enderror
+                                        </div>
+                                    </div>
+                                    <div class="col-12">
+                                        <div class="input-style-1">
+                                            <label>Email</label>
+                                            <input id="email" type="email"
+                                                   class="form-control @error('email') is-invalid @enderror"
+                                                   name="email" value="{{ old('email') }}" required
+                                                   autocomplete="email">
+
+                                            @error('email')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                            @enderror
+                                        </div>
+                                    </div>
+                                    <div class="col-12">
+                                        <div class="input-style-1">
+                                            <label>Password</label>
+                                            <input id="password" type="password"
+                                                   class="form-control @error('password') is-invalid @enderror"
+                                                   name="password" required autocomplete="new-password">
+                                            @error('password')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                            @enderror
+                                        </div>
+                                    </div>
+                                    <div class="col-12">
+                                        <div class="input-style-1">
+                                            <label>{{ __('Confirm Password') }}</label>
+                                            <input id="password-confirm" type="password" class="form-control"
+                                                   name="password_confirmation" required autocomplete="new-password">
+                                        </div>
+                                    </div>
+                                    <div class="col-12">
+                                        <div class="button-group d-flex justify-content-center flex-wrap">
+                                            <button class="main-btn primary-btn btn-hover w-100 text-center">
+                                                Register
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                            <div class="singup-option pt-40">
+                                <p class="text-sm text-medium text-dark text-center">
+                                    Already have an account? <a href="{{ route('login') }}">Login</a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
+
+<script src="{{ url("js/bootstrap.bundle.min.js") }}"></script>
+<script src="{{ url("js/main.js") }}"></script>
+</body>
+
+</html>
